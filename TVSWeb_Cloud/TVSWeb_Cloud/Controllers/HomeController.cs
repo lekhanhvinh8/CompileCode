190e2c4c7c47d745565eceffc8d91436edf44c96
Vinh <dotkichvz58@gmail.com>
2021-01-02 23:56:09 +0700

    initial commit

---
+++ TVSWeb_Cloud/TVSWeb_Cloud/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using TVSWeb_Cloud.Models;
+using System.Data.Entity;
+using TVSWeb_Cloud.Models.dtos;
+
+namespace TVSWeb_Cloud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TVSContext _context;
+
+        public HomeController()
+        {
+            _context = new TVSContext();
+            CommandPrompt.ExecuteCommand("docker run --rm -it -d -v " + ConfigurationDocker.StoragePathInHost + ":" + ConfigurationDocker.StoragePathInContainer + " --name " + ConfigurationDocker.MonoContainerName  + " mono");
+            CommandPrompt.ExecuteCommand("docker run --rm -it -d -v " + ConfigurationDocker.StoragePathInHost + ":" + ConfigurationDocker.StoragePathInContainer + " --name " + ConfigurationDocker.OpenjdkContainerName + " openjdk");
+
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Choosejava()
+        {
+            List<Question> questions = _context.Questions.Where(q => q.Type.Name == "java").ToList();
+            return View(questions);
+        }
+        public ActionResult Choosecsharp()
+        {
+            List<Question> questions = _context.Questions.Where(q => q.Type.Name == "c#").ToList();
+            return View(questions);
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult CodeScreen(int questionID)
+        {
+            var question = this._context.Questions.Include(q => q.Type).SingleOrDefault(q => q.QuestionID == questionID);
+
+            if (question == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return View(new QuestionDto(question));
+        }
+    }
+}
---
+++ TVSWeb_Cloud/TVSWeb_Cloud/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TVSWeb_Cloud.Models
+{
+    public class Question
+    {
+        public int QuestionID { get; set; }
+
+        public string QuestionTitle { get; set; }
+        public string QuestionContent { get; set; }
+
+        public TypeOfQuestion Type { get; set; }
+        public int TypeID { get; set; }
+    }
+}
